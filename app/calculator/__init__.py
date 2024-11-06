16b1d3b31779e7c6977bca9a7c7a77531d0cea0c
al732 <al732@njit.edu>
2024-11-06 18:56:09 -0500

    start of calculator, welcome message

---
+++ app/calculator/__init__.py
@@ -0,0 +1,11 @@
+"basic REPL calculator"
+
+"first integrate functions from opertions"
+from app.operations import addition, subtraction, multiplication, division
+
+def calculator():
+    """Basic REPL calculator that performs addition, subtraction, multiplication, and division."""
+    
+    # print welcome message upon program initialization 
+    print("Welcome to the calculator REPL! Type 'exit' to quit")
+ 
