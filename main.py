1e54a1604e992fbf63309a7c934592854e246e08
al732 <al732@njit.edu>
2024-11-06 20:02:53 -0500

    set up the main.py to run the program

---
+++ main.py
@@ -0,0 +1,9 @@
+#this the main python code page that will initialize and run the calculator
+
+from app.calculator import calculator
+
+if __name__ == "__main__": # the use of "__name__" such as __main__ indicates we are running
+                           # the program directly
+        
+
+        calculator()
