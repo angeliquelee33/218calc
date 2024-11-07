18f2c5fb9c70fab045a0ca9ab3b7fb2f1f8b5cf0
al732 <al732@njit.edu>
2024-11-07 10:52:29 -0500

    deleted useless file in wrong place

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,1 @@
-"addition function, with floats for decimals"
 
-def addition(a: float, b: float) -> float:
-    return a + b 
--- app/calculator/__init__.py
+++ app/calculator/__init__.py
@@ -1,7 +1,7 @@
 "basic REPL calculator"
 
 "first integrate functions from opertions"
-from app.operations import addition, subtraction, multiplication, division
+from app.operations import addition, subtraction, multiplication, division 
 
 def calculator():
     """Basic REPL calculator that performs addition, subtraction, multiplication, and division."""
